1b6ad7670127f5e21b4bf247f0f8e2050c07da9c
Foureyes(Douglas) <87150906+dlsr00t@users.noreply.github.com>
2024-02-23 17:48:30 -0300

    23/02/2024

--- todo.js
+++ todo.js
@@ -1,12 +1,65 @@
 function novaTarefa(){
-    let botaoCriar = document.createElement("button");
-    botaoCriar.id = "teste-botao";
-    let textoBotaoCriar = document.createTextNode("criar");
-    botaoCriar.appendChild(textoBotaoCriar);
+
+    if (document.getElementById("nova-tarefa1") == null){
+
+        let botaoCriar = document.createElement("button");
+        botaoCriar.id = "botao-criar";
+        let textoBotaoCriar = document.createTextNode("criar");
+        botaoCriar.appendChild(textoBotaoCriar);
+        
+
+        let criarNovaTarefa = document.createElement("input");
+        criarNovaTarefa.id = "nova-tarefa1" //*Eu preciso fazer a tarefa mudar de id, se não todas vão ter o mesmo id
+        // var criarNovaTarefa = document.createElement("input");
+        // criarNovaTarefa.id = "nova-tarefa" //*Eu preciso fazer a tarefa mudar de id, se não todas vão ter o mesmo id
+        
+
+        let teste = document.getElementById("nova-tarefa1");
+        console.log(teste);
+
+        //let criarTextoDaTarefa = document.createTextNode("fazer coco");
+        //criarNovaTarefa.appendChild(criarTextoDaTarefa);
+        
+        let adicionarNaDiv = document.getElementById("div-de-tarefas");
+        adicionarNaDiv.appendChild(criarNovaTarefa);
+        adicionarNaDiv.appendChild(botaoCriar);
+        //limitadorDeTarefas()
+        //console.log(document.getElementById("asdf"))
+
+    }else{
+        var contadorNovaTarefa = 2;
+        while(document.getElementById("nova-tarefa1") != null){
+            if(document.getElementById(`nova-tarefa${contadorNovaTarefa}`) == null){
+                let botaoCriar = document.createElement("button");
+                botaoCriar.id = "botao-criar";
+
+                let textoBotaoCriar = document.createTextNode("criar");
+                botaoCriar.appendChild(textoBotaoCriar);
+
+                let criarNovaTarefa = document.createElement("input");
+                criarNovaTarefa.id = `nova-tarefa${contadorNovaTarefa}`;
+
+                let teste = document.getElementById("nova-tarefa1");
+                console.log(teste);
+        
+
+                let adicionarNaDiv = document.getElementById("div-de-tarefas");
+                adicionarNaDiv.appendChild(criarNovaTarefa);
+                adicionarNaDiv.appendChild(botaoCriar);
+                break
+            }else{
+                contadorNovaTarefa+=1;
+            }
+        }
+    }
+    /*
+    var criarNovaTarefa = document.createElement("input");
+    criarNovaTarefa.id = "nova-tarefa" //*Eu preciso fazer a tarefa mudar de id, se não todas vão ter o mesmo id
     
-    let criarNovaTarefa = document.createElement("input");
-    criarNovaTarefa.id = "teste" //Eu preciso fazer a tarefa mudar de id, se não todas vão ter o mesmo id
-    
+
+    let teste = document.getElementById("nova-tarefa");
+    console.log(teste);
+
     //let criarTextoDaTarefa = document.createTextNode("fazer coco");
     //criarNovaTarefa.appendChild(criarTextoDaTarefa);
     
@@ -15,15 +68,17 @@ function novaTarefa(){
     adicionarNaDiv.appendChild(botaoCriar);
     //limitadorDeTarefas()
     //console.log(document.getElementById("asdf"))
+    */
 }
 
 function criarTarefa(){
-    //console.log(document.getElementById("teste").value)
+    //console.log(document.getElementById("nova-tarefa").value
     
+
     let label = document.createElement("label"); 
-    label.innerText = document.getElementById("teste").value; //! LINHA DE CODIGO PROBLEMATICA
+    label.innerText = document.getElementById(`nova-tarefa${contadorNovaTarefa2}`).value; //! LINHA DE CODIGO PROBLEMATICA
     
-    let removerBotao = document.getElementById("teste-botao");
+    let removerBotao = document.getElementById("botao-criar");
 
     let quebrarUmaLinha = document.createElement("br");
 
@@ -31,8 +86,12 @@ function criarTarefa(){
     adicionarNaDiv2.removeChild(removerBotao);
     adicionarNaDiv2.appendChild(label);
     adicionarNaDiv2.appendChild(quebrarUmaLinha);
-    document.getElementById("teste").type = "checkbox"; //! LINHA DE CODIGO PROBLEMATICA
+    document.getElementById(`nova-tarefa${contadorNovaTarefa2}`).type = "checkbox"; //! LINHA DE CODIGO PROBLEMATICA
+    contadorNovaTarefa2+=1;
+    console.log(contadorNovaTarefa2)
     return "proximo"
+    
+
 }
 
 function criarId(){
@@ -45,9 +104,9 @@ function criarId(){
 
 function limitadorDeTarefas(){
     
-    if (document.getElementById("teste-botao") == null){
+    if (document.getElementById("botao-criar") == null){
         novaTarefa()
-        let limitador = document.getElementById("teste-botao").addEventListener("click", criarTarefa);
+        let limitador = document.getElementById("botao-criar").addEventListener("click", criarTarefa);
         
     }else{
         alert("Você só pode criar uma tarefa de cada vez!!!")
@@ -56,5 +115,5 @@ function limitadorDeTarefas(){
 }
 
 document.getElementById("novaTarefa").addEventListener("click", limitadorDeTarefas);
-//document.getElementById("teste-botao").addEventListener("click", criarTarefa);
-
+//document.getElementById("botao-criar").addEventListener("click", criarTarefa);
+var contadorNovaTarefa2 = 1;
